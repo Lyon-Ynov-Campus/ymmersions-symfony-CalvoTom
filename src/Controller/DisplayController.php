74f9d2efe211a1b90852d65c5a944978c0951d6f
Tom <131645934+CalvoTom@users.noreply.github.com>
2025-02-05 12:18:48 +0100

    add siplay for tournament

--- src/Controller/DisplayController.php
+++ src/Controller/DisplayController.php
@@ -4,18 +4,37 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\TOURNAMENTRepository;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\TOURNAMENT;
 
 final class DisplayController extends AbstractController
 {
     #[Route('/display', name: 'app_display')]
     public function index(TOURNAMENTRepository $tournamentRepository): Response
     {
-        $tournaments=$tournamentRepository->findAll();
+        // Récupère tous les tournois
+        $tournaments = $tournamentRepository->findAll();
         return $this->render('display/index.html.twig', [
             'controller_name' => 'DisplayController',
             'tournaments' => $tournaments
+        ]);
+    }
+
+    #[Route('/tournaments/{id}', name: 'app_tournament_view')]
+    public function show(int $id, TOURNAMENTRepository $tournamentRepository): Response
+    {
+        // Recherche un tournoi par son id
+        $tournament = $tournamentRepository->find($id);
+
+        if (!$tournament) {
+            // Si aucun tournoi n'est trouvé, rediriger vers la page principale
+            throw $this->createNotFoundException('Tournament not found');
+        }
+
+        return $this->render('display/tournament_view.html.twig', [
+            'tournament' => $tournament
         ]);
     }
 }
